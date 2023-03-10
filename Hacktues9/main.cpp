57bcb21e1547e746634609008688769e2be359a5
AngelinaSvetlozarova <127393773+AngelinaSvetlozarova@users.noreply.github.com>
2023-03-10 09:32:32 +0200

    spaces fixed

--- Hacktues9/main.cpp
+++ Hacktues9/main.cpp
@@ -1,7 +1,10 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <string>
-#include <pch.h>
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
 
 #define MAX_PASSWORDS 10
 #define MAX_PASSWORD_LENGTH 20
@@ -17,13 +20,15 @@ int main() {
 
     printf("Welcome to Password Safe\n");
     printf("Enter the master password: ");
-    scanf("%s", master_password);
+    //scanf("%s", master_password);
+    cin.getline(master_password, MAX_PASSWORD_LENGTH);
 
     while (1) {
         if (!is_logged_in) {
             char input_password[MAX_PASSWORD_LENGTH];
             printf("Enter the master password: ");
-            scanf("%s", input_password);
+            //scanf("%s", input_password);
+            cin.getline(input_password, MAX_PASSWORD_LENGTH);
 
             if (strcmp(input_password, master_password) == 0) {
                 printf("Logged in successfully!\n");
@@ -42,7 +47,7 @@ int main() {
         else {
             printf("Choose an option:\n");
             printf("1. Add password\n");
-            printf("2. Show passwords\n");
+            printf("2. Show password\n");
             printf("3. Exit\n");
 
             int choice;
@@ -54,9 +59,14 @@ int main() {
                 }
                 else {
                     printf("Enter the new name: ");
-                    scanf("%s", name[num_passwords]);
+                    //scanf("%s", name[num_passwords]);
+                    getchar();
+
+                    cin.getline(name[num_passwords], MAX_NAME_LENGTH);
+
                     printf("Enter the new password: ");
-                    scanf("%s", passwords[num_passwords]);
+                    //scanf("%s", passwords[num_passwords]);
+                    cin.getline(passwords[num_passwords], MAX_PASSWORD_LENGTH);
                     num_passwords++;
                 }
             }
@@ -64,7 +74,10 @@ int main() {
                 char c_name[MAX_NAME_LENGTH];
                 int flag = 0;
                 printf("Enter name: ");
-                scanf("%s", c_name);
+                //scanf("%s", c_name);
+                getchar();
+
+                cin.getline(c_name, MAX_NAME_LENGTH);
 
                 for (int i = 0; i < num_passwords; i++)
                 {
